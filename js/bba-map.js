f7e6a0759330ebbd4145172c679432891a597212
itagaki <itagaki.shintaro@gmail.com>
2014-11-09 21:16:14 +0900

    とりあえず版完成

---
+++ js/bba-map.js
@@ -0,0 +1,58 @@
+var mymap;
+
+google.maps.event.addDomListener(window, 'load', handleLoadGoogleMap);
+
+function handleLoadGoogleMap()
+{
+  var mapOptions = {
+    center: new google.maps.LatLng(35.6874916666667,139.766158333333),
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    disableDefaultUI: true
+  };
+  mymap = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+  d3.json('data/line_data.geojson', function(error, data)
+  {
+    if (error != null)
+    {
+      console.log(error);
+      return;
+    }
+    mymap.data.addGeoJson(data);
+  });
+  d3.json('data/station_latlon.json', function(error, data)
+  {
+    if (error != null)
+    {
+      console.log(error);
+      return;
+    }
+    data.forEach(function(d)
+    {
+      var markerOpts =
+      {
+       position: new google.maps.LatLng(d.lat,d.lon),
+       draggable: false,
+       raiseOnDrag: false,
+       map: mymap,
+       labelContent: d.name,
+       labelAnchor: new google.maps.Point(22, 0),
+       labelClass: 'labels',
+       labelStyle: {opacity: 1.0},
+      };
+      var marker = new MarkerWithLabel(markerOpts);
+      google.maps.event.addListener(marker, 'click', function()
+      {
+        show(d.id);
+      });
+    });
+  });
+}
+
+// ここから出力
+function show(station)　{
+  destination = station;
+  getBlackCars();
+  location.href = '#result';
+}
--- js/bba.js
+++ js/bba.js
@@ -3,144 +3,122 @@
 var URL = 'https://api.tokyometroapp.jp/api/v2/datapoints';
 var TOKEN = '22b4f2f8dd953bb676f7145b0777fb20e7d288e5f3662dd3837ccb6854eefadd';
 // 仮設定。本当はユーザーの入力からとる。
-var destination = 'odpt.StationFacility:TokyoMetro.KokkaiGijidomae';
+var destination = '';
 var railway = 'odpt.Railway:TokyoMetro.Marunouchi';
 var railDirection = 'odpt.RailDirection:TokyoMetro.Ikebukuro';
 
-
-
-
-$(function() {
-  getBlackCars();
-  // getRailways();
-});
-
 function getBlackCars() {
-  $("#search").click(function() {
-    $('#result .car').remove();
-    $('#result h2').remove();
+  $('#result *').remove();
 
-    $.ajax({
-      url: URL,
-      data: {
-        'acl:consumerKey': TOKEN,
-        'rdf:type': 'odpt:StationFacility',
-        'owl:sameAs': destination
+  $.ajax({
+    url: URL,
+    data: {
+      'acl:consumerKey': TOKEN,
+      'rdf:type': 'odpt:StationFacility',
+      'owl:sameAs': destination
+    }
+  }).done(function(facility) {
+    var platformInformation = facility[0]['odpt:platformInformation'];
+    // 路線、進行方向、車両編成の一覧を取得
+    var targets = [];
+    platformInformation.forEach(function(info){
+      var target = [ info['odpt:railway'], info['odpt:railDirection'], info['odpt:carComposition']];
+      if(targets.join().indexOf(target.join()) < 0){
+        targets.push(target);
       }
-    }).done(function(facility) {
-      var platformInformation = facility[0]['odpt:platformInformation'];
-      // 路線、進行方向、車両編成の一覧を取得
-      var targets = [];
+    });
+
+    // 駅名の表示
+    setStationName(targets[0][0], destination);
+
+    // 路線、進行方向ごとに、バリアフリーの有無を判定し、図を表示
+    targets.forEach(function(target){
+      var railway = target[0];
+      var railDirection = target[1];
+      var carComposition = target[2];
+      var cars = [];
+      for(var i = 1; i <= carComposition; i++){
+        cars[i] = 0;
+      }
       platformInformation.forEach(function(info){
-        var target = [ info['odpt:railway'], info['odpt:railDirection'], info['odpt:carComposition']];
-        if(targets.join().indexOf(target.join()) < 0){
-          targets.push(target);
+        if( info['odpt:railway'] === railway
+            && info['odpt:railDirection'] === railDirection
+            && info['odpt:barrierfreeFacility'] ){
+          info['odpt:barrierfreeFacility'].forEach(function(bff){
+            // エレベーターかエスカレーターのどちらかがある場合
+            if( bff.search(/Escalator/) > 0 || bff.search(/Elevator/) > 0 ){
+              cars[Number(info['odpt:carNumber'])] += 1;
+            }
+          });
         }
       });
 
-      targets.forEach(function(target){
-        var railway = target[0];
-        var railDirection = target[1];
-        var carComposition = target[2];
-        var cars = [];
-        for(var i = 1; i <= carComposition; i++){
-          cars[i] = 0;
-        }
-        platformInformation.forEach(function(info){
-          if( info['odpt:railway'] === railway
-              && info['odpt:railDirection'] === railDirection
-              && info['odpt:barrierfreeFacility'] ){
-            info['odpt:barrierfreeFacility'].forEach(function(bff){
-              // エレベーターかエスカレーターのどちらかがある場合
-              if( bff.search(/Escalator/) > 0 || bff.search(/Elevator/) > 0 ){
-                cars[Number(info['odpt:carNumber'])] += 1;
-              }
-            });
-          }
-        });
+      // 路線、進行方向のタグを表示
+      setTag(railway, railDirection);
 
-        // バリアフリー施設があるときは白とないときは黒で図を表示
-        $('#result').append('<h2>' + railway + ',' + railDirection + '</h2>');
-
-        for(var i = 1; i <= carComposition; i++){
-          if( cars[i] === 0 ){
-            $('#result').append('<div class="car black">' + i + '</div>');
-          } else {
-            $('#result').append('<div class="car white">' + i + '</div>');
-          } 
-        }
-
-
-
-      });
-
-
-
-
+      // バリアフリー施設があるときは白とないときは黒で図を表示
+      for(var i = 1; i <= carComposition; i++){
+        if( cars[i] === 0 ){
+          $('#result').append('<span class="car black">' + i + '</span>');
+        } else {
+          $('#result').append('<span class="car white">' + i + '</span>');
+        } 
+      }
     });
   });
 }
 
-        // var carComposition = Number( platformInformation[0]['odpt:carComposition'] );
-        // var cars = new Array(carComposition + 1);
-        // for(var i = 1; i <= carComposition; i++){
-        //   cars[i] = 0;
-        // }
+function setStationName(railway, destination){
+  var railway_split = railway.split('.');
+  var destination_split = destination.split('.');
+  var sameAs = 'odpt.Station:TokyoMetro.' + railway_split[railway_split.length - 1] + '.' + destination_split[destination_split.length - 1];
 
+  $.ajax({
+    url: URL,
+    async: false,
+    data: {
+      'acl:consumerKey': TOKEN,
+      'rdf:type': 'odpt:Station',
+      'owl:sameAs': sameAs
+    }
+  }).done(function(station) {
+    $('#result').prepend('<div><span class="label label-primary">' + station[0]['dc:title'] + '駅</span></div>');
+  });
+}
 
-        // var platforms = [];
-        // // 路線、進行方向ごとにプラットフォームの情報をとる
-        // platformInformation.forEach(function(info){
-        //   var platform = {'odpt:railway': info['odpt:railway'], 
-        //                   'odpt:carComposition': info['odpt:carComposition'], 
-        //                   'odpt:railDirection': info['odpt:railDirection'],
-        //                   'cars': new Array(Number(info['odpt:carComposition']))};
+function setTag(railway, railDirection){
+  // 路線名を取得
+  var railway_name = '';
+  $.ajax({
+    url: URL,
+    async: false,
+    data: {
+      'acl:consumerKey': TOKEN,
+      'rdf:type': 'odpt:Railway',
+      'owl:sameAs': railway
+    }
+  }).done(function(railway) {
+    railway_name = railway[0]['dc:title'];
+    // $('#result').append('<div><span class="label label-default">' + railway[0]['dc:title'] + '線</span></div>');
+  });
 
-        //   if( JSON.stringify(platforms).indexOf(JSON.stringify(platform)) < 0 ){
-        //     platforms.push(platform);
-        //   }
-        // }
+  // 進行方向名を取得
+  var railDirection_name = '';
+  var railway_split = railway.split('.');
+  var railDirection_split = railDirection.split('.');
+  var sameAs = 'odpt.Station:TokyoMetro.' + railway_split[railway_split.length - 1] + '.' + railDirection_split[railDirection_split.length - 1];
 
-        // 路線、進行方向、車両ごとにバリアフリー施設の有無を調べる
-
-
-          
-        
-          // // 路線と進行方向が指定どおりで、バリアフリー施設がある場合のみ処理する
-          // if( info['odpt:railway'] === railway
-          //   && info['odpt:railDirection'] === railDirection
-          //   && info['odpt:barrierfreeFacility'] ){
-
-          //   info['odpt:barrierfreeFacility'].forEach(function(bff){
-          //     // エレベーターかエスカレーターのどちらかがある場合
-          //     if( bff.search(/Escalator/) > 0 || bff.search(/Elevator/) > 0 ){
-          //       cars[Number(info['odpt:carNumber'])] += 1;
-          //     }
-          //   });
-
-          // }
-
-
-// // 仮設定。本当はユーザーの入力からとる。
-// var dest_name_jp = '上野';
-
-// function getRailways() {
-//   $("#search").click(function() {
-//     $.ajax({
-//       url: URL,
-//       data: {
-//         'acl:consumerKey': TOKEN,
-//         'rdf:type': 'odpt:Station',
-//         'dc:title': dest_name_jp
-//       }
-//     }).done(function(same_stations) {
-//       var railways = [];
-//       same_stations.forEach(function(station){
-//         if(station['dc:title'] === dest_name_jp){
-//           railways.push(station['odpt:railway']);
-//         }
-//       });
-//       console.log(railways);
-//     });
-//   });
-// }
+  $.ajax({
+    url: URL,
+    async: false,
+    data: {
+      'acl:consumerKey': TOKEN,
+      'rdf:type': 'odpt:Station',
+      'owl:sameAs': sameAs
+    }
+  }).done(function(station) {
+    railDirection_name = station[0]['dc:title'];
+    // $('#result').append('<div><span class="label label-default">' + station[0]['dc:title'] + '駅方面行き</span></div>');
+  });
+  $('#result').append('<div class="tag"><span class="label label-default">' + railway_name + '線</span> <span class="label label-default">' + railDirection_name + '駅方面行き</span></div>');
+}
